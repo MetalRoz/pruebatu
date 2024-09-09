83c8ec7aec5b7c1b041b3f8349a04adca8e8197b
Wilson <wilsonmiguel2003@gmail.com>
2024-09-09 12:59:42 -0400

    trabajando vista de status en scanner

--- components/Scanner.tsx
+++ components/Scanner.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import {
-  ImageBackground,
-  StyleSheet,
-  Alert,
   Button,
   Text,
   View,
+  StyleSheet,
+  ActivityIndicator,
 } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 import {
   startScan,
@@ -19,8 +19,35 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Scanner() {
   const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<any>(null); // Estado para almacenar la respuesta de la API
+  const [checkInStatus, setCheckInStatus] = useState<number | null>(null); // Estado para el estado del check-in
+  const [forceRender, setForceRender] = useState(0); // Estado adicional para forzar re-renderizado
+  const [isLoading, setIsLoading] = useState(false); // Estado para controlar el loading
+
+  // Verifica si el ticket escaneado coincide con el último almacenado
+  const verifyLastTicketId = async (ticketId: string) => {
+    const lastTicketId = await AsyncStorage.getItem("lastScannedTicket");
+    return lastTicketId === ticketId;
+  };
+
+  // Almacena el último ticket escaneado en AsyncStorage
+  const storeLastTicketId = async (ticketId: string) => {
+    await AsyncStorage.setItem("lastScannedTicket", ticketId);
+  };
 
   const checkIn = async (ticketId: any) => {
+    setIsLoading(true); // Iniciar loading
+
+    const isLastTicket = await verifyLastTicketId(ticketId);
+
+    if (isLastTicket) {
+      // Si el ticket coincide con el último almacenado, mostrar pantalla anaranjada
+      setCheckInStatus(2);
+      setIsLoading(false); // Finalizar loading
+      setForceRender((prev) => prev + 1);
+      return;
+    }
+
     const userData = await AsyncStorage.getItem("userData");
     const event_data = await AsyncStorage.getItem("event_data");
     if (userData && event_data !== null) {
@@ -35,14 +62,32 @@ export default function Scanner() {
       const apiCheckIn = `https://pruebatu.com/api/v2/ticketcode/${ticketId}/${eventId}`;
       try {
         const response = await fetch(apiCheckIn, options);
-        if (!response.ok) {
-          throw new Error("Error:" + `${response.status}`);
+        const responseData = await response.json();
+
+        setData(responseData);
+
+        // Verificar el contenido de responseData.message
+        if (
+          responseData.message.includes(
+            "Este ticket ya se encuentra en el evento"
+          )
+        ) {
+          setCheckInStatus(1);
+          await storeLastTicketId(ticketId); // Almacenar el ticketId si es válido
+        } else if (
+          responseData.message.includes("Este ticket no es para este evento")
+        ) {
+          setCheckInStatus(0);
         }
-        const data = await response.json();
-        Alert.alert(data.message);
-        console.log("Respuesta del CHECKIN:", data);
+
+        // Forzamos un re-render después de cambiar el estado
+        setForceRender((prev) => prev + 1);
+
+        console.log("Respuesta del CHECKIN:", responseData);
       } catch (error) {
         console.error("Error en la solicitud:", error);
+      } finally {
+        setIsLoading(false); // Finalizar loading
       }
     }
   };
@@ -73,75 +118,134 @@ export default function Scanner() {
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Iniciar scanner" onPress={iniciar}/>
-      <Button title="Detener scanner" onPress={detener}/>
+    <View style={styles.container} key={forceRender}>
+      {isLoading ? (
+        // Mostrar indicador de carga mientras se realiza el check-in
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text style={styles.loadingText}>Procesando check-in...</Text>
+        </View>
+      ) : checkInStatus === null ? (
+        <>
+          <Button title="Iniciar scanner" onPress={iniciar} />
+          <Button title="Detener scanner" onPress={detener} />
+
+        </>
+      ) : (
+        <View
+          style={[
+            styles.fullScreenContainer,
+            checkInStatus === 1
+              ? styles.validCheckin
+              : checkInStatus === 0
+              ? styles.invalidCheckin
+              : styles.alreadyCheckedIn, // Estilo para el estado anaranjado
+          ]}
+        >
+          {checkInStatus === 1 ? (
+            <>
+              <FontAwesome name="check-circle" size={80} color="white" />
+              <Text style={styles.resultText}>Checked in!</Text>
+              <Text style={styles.vipText}>VIP</Text>
+              <View style={styles.infoContainer}>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Name</Text>
+                  <Text style={styles.value}>Karna Ballefant</Text>
+                </View>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Title</Text>
+                  <Text style={styles.value}>Assistant Manager</Text>
+                </View>
+                <View style={styles.infoRow}>
+                  <Text style={styles.label}>Company</Text>
+                  <Text style={styles.value}>Yodo</Text>
+                </View>
+              </View>
+            </>
+          ) : checkInStatus === 0 ? (
+            <>
+              <FontAwesome name="times-circle" size={80} color="white" />
+              <Text style={styles.resultText}>{data.message}</Text>
+            </>
+          ) : (
+            // Pantalla para ticket ya escaneado
+            <>
+              <FontAwesome
+                name="exclamation-triangle"
+                size={80}
+                color="white"
+              />
+              <Text style={styles.resultText}>
+                Este ticket coincide con el último escaneo.
+              </Text>
+            </>
+          )}
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 100,
-    width: 200,
-    margin: "auto",
-    display: "flex",
-    gap: 20
-  },
-  imageBackground: {
-    width: "100%",
-    height: 400, // Ajusta según el tamaño necesario
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  overlay: {
-    backgroundColor: "rgba(0, 0, 0, 0.5)", // Fondo semitransparente
-    padding: 20,
-    borderRadius: 10,
+  loadingContainer: {
+    justifyContent: "center",
     alignItems: "center",
   },
-  entryGateText: {
-    color: "#fff",
+  loadingText: {
+    marginTop: 10,
     fontSize: 18,
+    color: "#000",
   },
-  gateText: {
+  fullScreenContainer: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  validCheckin: {
+    backgroundColor: "green",
+  },
+  invalidCheckin: {
+    backgroundColor: "red",
+  },
+  alreadyCheckedIn: {
+    backgroundColor: "orange",
+  },
+  resultText: {
     color: "#fff",
-    fontSize: 24,
+    fontSize: 32,
     fontWeight: "bold",
+    marginTop: 20,
+    textAlign: "center",
   },
-  row: {
-    flexDirection: "row",
+  vipText: {
+    color: "#fff",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginVertical: 20,
+  },
+  infoContainer: {
     marginTop: 20,
   },
-  column: {
-    marginHorizontal: 10,
-    alignItems: "center",
+  infoRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+    marginVertical: 5,
   },
   label: {
     color: "#fff",
-    fontSize: 16,
+    fontSize: 18,
   },
   value: {
     color: "#fff",
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
-  },
-
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-    marginTop: 50,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
   },
 });
